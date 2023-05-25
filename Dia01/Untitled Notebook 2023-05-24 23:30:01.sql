dbdfb177abbe77915213ddfe0ac2db771e9634f6
renata.brito93@gmail.com <renata.brito93@gmail.com>
2023-05-25 02:35:23 +0000

    Olá mundo do primeiro dia

---
+++ Dia01/Untitled Notebook 2023-05-24 23:30:01.sql
@@ -0,0 +1,11 @@
+-- Databricks notebook source
+SELECT 'OLÁ MUNDO!'
+
+-- COMMAND ----------
+
+SELECT *                         -- selecione todasa as colunas
+FROM silver.pizza_query.pedido   -- da tabela silver.pizza_query.pedido
+
+-- COMMAND ----------
+
+
